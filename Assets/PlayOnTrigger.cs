ccb013f3afe2158f80a67d05aef015174718f8e4
Scott Beale <sbeale3@gatech.edu>
2016-07-20 20:38:55 -0400

    ambient sounds added

---
+++ Assets/PlayOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOnTrigger : MonoBehaviour {
+
+    public AudioSource toPlay;
+
+    //http://docs.unity3d.com/ScriptReference/Collider.OnTriggerEnter.html
+	void OnTriggerEnter(Collider other) {
+
+		//ways to check to see if a specific object has entered the trigger:
+
+		//Presence of a Component:
+		//checking to see if the object we collided with has a CharacterController
+
+		CharacterController controller = other.gameObject.GetComponent<CharacterController>();
+		if (controller != null) //only change color when character controller found
+		{
+			if(!toPlay.isPlaying) {
+                toPlay.Play();
+            }
+		}
+    }
+}
