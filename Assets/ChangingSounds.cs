82fbea9e5f6864db1cce374bc92ee9331aa3d485
Mitch Leff <mal8690@gmail.com>
2016-07-21 00:57:11 -0400

    Added footsteps, converted sounds to mp3, created changingsounds script

---
+++ Assets/ChangingSounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangingSounds : MonoBehaviour {
+
+	public AudioSource source;
+	public CharacterController character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
